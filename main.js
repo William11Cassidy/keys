7382933f272e621e9237a047b53e7f3d8d48ae94
William11Cassidy <121349385+William11Cassidy@users.noreply.github.com>
2023-04-11 15:37:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,70 @@
-color = "blue";
-width_of_line = 2;
-canvas=document.getElementById("myCanvas")
-ctx=canvas.getContext("2d")
-width=screen.width
-newwidth=width-70
-newhight=screen.height-300
-if(width<992){
+canvas = document.getElementById("myCanvas")
+img_width = 300;
+ctx = canvas.getContext("2d")
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image();
+	img_imgTag.onload = uploadimg;
+	img_imgTag.src = img_image;
 }
-var last_position_of_x, last_position_of_y;
 
-canvas.addEventListener("touchstart", my_touchstart);
+function uploadimg() {
 
-function my_touchstart(e){
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown)
+function my_keydown(e) {
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
+		alphabetkey()
+	}
+	else if ((keyPressed >= 48 && keyPressed <= 57)) {
+		numberkey()
+	}
+	else if (keyPressed >= 37 && keyPressed <= 40) {
+		arrowkey()
+	}
+	else if (keyPressed == 17 || keyPressed == 18 || keyPressed == 27) {
+		specialkey()
+	}
+	else {
+		otherkey();
+		document.getElementById("d1").innerHTML = "You pressed symbol or other key";
+	}
+
+
 
 }
 
-canvas.addEventListener("touchmove", my_touchmove);
-function my_touchmove(e)
-{
+function alphabetkey() {
+	img_image = "Alpkey.png";
+	add();
 
-
-
-     current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-     current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
-
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width_of_line;
-
-    ctx.moveTo(last_position_of_x, last_position_of_y);
-
-    console.log("Current position of x and y coordinates = ");
-    console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
-    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
-    ctx.stroke();
-    
-
-    last_position_of_x = current_position_of_touch_x; 
-    last_position_of_y = current_position_of_touch_y;
 }
+function numberkey() {
+	img_image = "numkey.png";
+	add();
+}
+function arrowkey() {
+	img_image = "Arrkey.png";
+	add();
+}
+
+function specialkey() {
+	img_image = "spkey.png";
+	add();
+}
+function otherkey() {
+	img_image = "otherkey.png";
+	add();
+}
+
